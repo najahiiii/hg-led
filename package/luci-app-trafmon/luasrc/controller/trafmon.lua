e04e95b788fd79987cd8b2bf59b942713ce5a083
Ahmad Thoriq Najahi <najahi@zephyrus.id>
2025-10-10 16:51:37 +0700

    feat: move app into system tabs
    Signed-off-by: Ahmad Thoriq Najahi <najahi@zephyrus.id>

--- package/luci-app-trafmon/luasrc/controller/trafmon.lua
+++ package/luci-app-trafmon/luasrc/controller/trafmon.lua
@@ -5,9 +5,9 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "trafmon"},
+	entry({"admin", "system", "trafmon"},
 		firstchild(), _("TrafMon"), 60).dependent = false
 
-	entry({"admin", "services", "trafmon", "config"},
+	entry({"admin", "system", "trafmon", "config"},
 		view("trafmon/config"), _("Configuration"), 1).leaf = true
 end
